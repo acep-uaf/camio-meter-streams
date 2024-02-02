5ba670bd796924263d135c69465a227ac3420d00
mnmah <nicolemah99@gmail.com>
2024-02-02 01:20:37 +0000

    check for secrets.json file

--- sel-meter-ftp-download.sh
+++ sel-meter-ftp-download.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-# FTP server details
-FTP_SERVER=$(jq -r '.ftp_server' secrets.json)
-FTP_USER=$(jq -r '.ftp_username' secrets.json)
-FTP_PASSWORD=$(jq -r '.ftp_password' secrets.json)
-FTP_REMOTE_PATH=$(jq -r '.ftp_remote_path' secrets.json)
-LOCAL_PATH=$(jq -r '.local_path' secrets.json)
+# Check if secrets.json exists in current directory
+if [ ! -f secrets.json ]; then
+	echo "Please enter the following details:"
+	read -p "Enter FTP username" FTP_USER
+	read -sp "Enter FTP password" FTP_PASSWORD
+	echo
+	read -p "Enter FTP server IP address: " FTP_SERVER
+	# Set default remote and local paths
+	FTP_REMOTE_PATH ="/"
+	LOCAL_PATH="/sel-735-downloads/"
+else
+	# FTP server details from secrets.json
+	FTP_SERVER=$(jq -r '.ftp_server' secrets.json)
+	FTP_USER=$(jq -r '.ftp_username' secrets.json)
+	FTP_PASSWORD=$(jq -r '.ftp_password' secrets.json)
+	FTP_REMOTE_PATH=$(jq -r '.ftp_remote_path' secrets.json)
+	LOCAL_PATH=$(jq -r '.local_path' secrets.json)
+fi
+echo "FTP Details:"
+echo "Server: $FTP_SERVER"
+echo "User: $FTP_USER"
+echo "Remote Path: $FTP_REMOTE_PATH"
+echo "Local Path: $LOCAL_PATH"
 
-echo $FTP_SERVER $FTP_USER $FTP_PASSWORD $FTP_REMOTE_PATH $LOCAL_PATH
-
-# Prompt for username and password
-#read -p "Enter FTP username: " FTP_USER
-#read -sp "Enter FTP password: " FTP_PASS
-#echo
+#TODO: Try catch to connect to meter
+#Return: Result
+# lftp -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER
 
 ## Create local directory if it doesn't exist
 ## TODO: Test for proper path
@@ -22,3 +36,4 @@ echo $FTP_SERVER $FTP_USER $FTP_PASSWORD $FTP_REMOTE_PATH $LOCAL_PATH
 ## Using lftp to mirror the directory
 ## TODO: Return Login success or failure
 #lftp -u "$FTP_USER,$FTP_PASS" -e "mirror --verbose --only-newer $FTP_REMOTE_PATH $LOCAL_PATH; quit" $FTP_SERVER
+
