63fffb560f8dae277345c41d68bc5e9543d03ad2
aloralove <agreer5@alaska.edu>
2024-02-07 02:23:02 +0000

    downloading from EVENTS to EVENTS locally

--- sel-meter-ftp-download.sh
+++ sel-meter-ftp-download.sh
@@ -1,51 +1,46 @@
 #!/bin/bash
 
+LOG_FILE="ftp_transfer.log"
 
-# Check if secrets.json exists in current directory
+# Log function definition
+log() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - $1" | tee -a "$LOG_FILE"
+}
+
+# Check if secrets.json exists and read FTP details from it
 if [ ! -f secrets.json ]; then
-	echo "Please enter the following details:"
-	read -p "Enter FTP username: " FTP_USER
-	read -sp "Enter FTP password: " FTP_PASSWORD
-	echo
-	read -p "Enter FTP server IP address: " FTP_SERVER
-	# Set default remote and local paths
-else
-	echo "secrets.json found"
-	# FTP server details from secrets.json
-	FTP_SERVER=$(jq -r '.ftp_server' secrets.json)
-	FTP_USER=$(jq -r '.ftp_username' secrets.json)
-	FTP_PASSWORD=$(jq -r '.ftp_password' secrets.json)
-	FTP_REMOTE_PATH=$(jq -r '.ftp_remote_path' secrets.json)
-	LOCAL_PATH=$(jq -r '.local_path' secrets.json)
+    echo "secrets.json file not found. Exiting."
+    exit 1
 fi
 
-echo "Remote Path: Attempting to download from $FTP_REMOTE_PATH"
+# FTP server details from secrets.json
+FTP_SERVER=$(jq -r '.ftp_server' secrets.json)
+FTP_USER=$(jq -r '.ftp_username' secrets.json)
+FTP_PASSWORD=$(jq -r '.ftp_password' secrets.json)
+FTP_REMOTE_PATH=$(jq -r '.ftp_remote_path' secrets.json)
+LOCAL_PATH=$(jq -r '.local_path' secrets.json)
 
+#log "Remote Path: $FTP_REMOTE_PATH"
+log "Local Path: $LOCAL_PATH"
+log "Remote Path: $FTP_REMOTE_PATH"
 ## Create local directory if it doesn't exist
 mkdir -p "$LOCAL_PATH"
 
-# Attempt to login and perform operations
-lftp "$FTP_SERVER" -e "bye" -u "$FTP_USER,$FTP_PASSWORD"
-login_status=$?
-
-if [ $login_status -ne 0 ]; then
-    echo "Failed to log in to the FTP server: $FTP_SERVER. Please check your credentials and server address."
-    exit 1
-else
-    echo "Successfully logged in to the FTP server. Proceeding with file operations."
-fi
-
-## FIXME: mirror not copying contents/correct permissions to local directory
-lftp "$FTP_SERVER" -u "$FTP_USER,$FTP_PASSWORD" <<EOF
-mirror $FTP_REMOTE_PATH $LOCAL_PATH
-quit
+# Start lftp session
+lftp -u "$FTP_USER,$FTP_PASSWORD" "$FTP_SERVER" <<EOF
+cd EVENTS
+lcd EVENTS
+mget *
+bye
 EOF
 
-# Check the status of the lftp operations
+# Check if the lftp operation was successful
 if [ $? -ne 0 ]; then
-    echo "Failed to complete the FTP operations."
+    log "Failed to list files from FTP server. Exiting."
     exit 1
-else
-    echo "FTP operations completed successfully."
-	echo "Files downloaded to ./$LOCAL_PATH."
 fi
+
+log "FTP download operations completed."
+
+# End of the script logging
+log "FTP download script completed."
