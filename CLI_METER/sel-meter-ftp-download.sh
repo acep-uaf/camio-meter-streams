f708f13ba43e8068fcd2d5514791a3ad273d5e73
aloralove <agreer5@alaska.edu>
2024-02-09 20:29:47 +0000

    added log error, added meter id and meter name in .env, also updated dir so files are saved in dir EVENTS/METER_ID/level0

--- CLI_METER/sel-meter-ftp-download.sh
+++ CLI_METER/sel-meter-ftp-download.sh
@@ -10,26 +10,35 @@ log() {
 # Load the .env file
 if [ -f .env ]; then
     export $(cat .env | xargs)
+else
+    log "Error: .env file not found. Exiting script."
+    echo "Error: .env file not found. Exiting script."
+    exit 1
 fi
 
 # Evnironment Variables 
-FTP_SERVER=$FTP_SERVER
-FTP_USER=$FTP_USER
-FTP_PASSWORD=$FTP_PASSWORD
-FTP_REMOTE_PATH=$FTP_REMOTE_PATH
+FTP_METER_SERVER_IP=$FTP_METER_SERVER_IP
+FTP_METER_NAME=$FTP_METER_NAME
+FTP_METER_ID=$FTP_METER_ID
+FTP_METER_USER=$FTP_METER_USER
+FTP_METER_USER_PASSWORD=$FTP_METER_USER_PASSWORD
+FTP_REMOTE_METER_PATH=$FTP_REMOTE_METER_PATH
 LOCAL_PATH=$LOCAL_PATH
 
 #log "Remote Path: $FTP_REMOTE_PATH"
 log "Local Path: $LOCAL_PATH"
-log "Remote Path: $FTP_REMOTE_PATH"
+log "Remote Path: $FTP_REMOTE_METER_PATH"
+
+DIR_PATH="$LOCAL_PATH/$FTP_METER_ID/level0"
+
 ## Create local directory if it doesn't exist
-mkdir -p "$LOCAL_PATH"
+mkdir -p "$DIR_PATH"
 
 # Start lftp session
-lftp -u "$FTP_USER,$FTP_PASSWORD" "$FTP_SERVER" <<EOF
-cd EVENTS
-lcd EVENTS
-mget *
+lftp -u "$FTP_METER_USER,$FTP_METER_USER_PASSWORD" "$FTP_METER_SERVER_IP" <<EOF
+cd EVENTS  
+lcd "$DIR_PATH"             
+mget *                       
 bye
 EOF
 
