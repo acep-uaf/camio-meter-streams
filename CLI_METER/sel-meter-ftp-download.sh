792fc2dd2c1ec5e8e24a041eb0d0291f8e4273e9
aloralove <agreer5@alaska.edu>
2024-02-09 22:36:02 +0000

    started basic wrapper script to connect scripts for data pipeline

--- CLI_METER/sel-meter-ftp-download.sh
+++ CLI_METER/sel-meter-ftp-download.sh
@@ -7,23 +7,6 @@ log() {
     echo "$(date +"%Y-%m-%d %H:%M:%S") - $1" | tee -a "$LOG_FILE"
 }
 
-# Load the .env file
-if [ -f .env ]; then
-    export $(cat .env | xargs)
-else
-    log "Error: .env file not found. Exiting script."
-    echo "Error: .env file not found. Exiting script."
-    exit 1
-fi
-
-# Evnironment Variables 
-FTP_METER_SERVER_IP=$FTP_METER_SERVER_IP
-FTP_METER_NAME=$FTP_METER_NAME
-FTP_METER_ID=$FTP_METER_ID
-FTP_METER_USER=$FTP_METER_USER
-FTP_METER_USER_PASSWORD=$FTP_METER_USER_PASSWORD
-FTP_REMOTE_METER_PATH=$FTP_REMOTE_METER_PATH
-LOCAL_PATH=$LOCAL_PATH
 
 #log "Remote Path: $FTP_REMOTE_PATH"
 log "Local Path: $LOCAL_PATH"
