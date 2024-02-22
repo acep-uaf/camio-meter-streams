ca88fced57507fbb9d41d00515c2beb902b9e920
nicolemah99 <nicolemah99@gmail.com>
2024-02-22 02:01:27 +0000

    log when connection failed to connect_to_mete.log

--- CLI_METER/connect_to_meter.sh
+++ CLI_METER/connect_to_meter.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 
-# This file is here to test connection to meter
+# Assume the log function is defined in another script and sourced here
+# source /path/to/your/log_function_script.sh
 
+# This file is here to test connection to meter
 LOG_FILE="connect_to_meter.log"
 
 # Logging in to the FTP server and checking the connection
 FTP_OUTPUT=$(ftp -inv $FTP_METER_SERVER_IP <<EOF
 user $FTP_METER_USER $FTP_METER_USER_PASSWORD
+ls
 bye
 EOF
 )
 
-# Check exit status of FTP command
-if [ $? -ne 0 ]; then
-  log "FTP connection test to meter failed." "ERROR" "$LOG_FILE"
-  echo "FTP connection test to meter failed."
-  exit 1
+# Check if specific FTP error messages are present
+if [[ "$FTP_OUTPUT" =~ "421 Service not available, closing control connection." && "$FTP_OUTPUT" =~ "Not connected." ]]; then
+    # Log diagnostic information if FTP connection failed
+    echo "The FTP service is not available, and the connection was not established."
+    log "FTP Server IP: $FTP_METER_SERVER_IP is not available." "ERROR" "$LOG_FILE"
+    # Handle the error, e.g., exit the script or try to reconnect
+    exit 1
 else
-  log "FTP connection test to meter succeeded." "SUCCESS" "$LOG_FILE"
-  echo "$FTP_OUTPUT" | awk '
-  BEGIN {
-    print "\n"
-    print "FTP Connection Report"
-    print "--------------------------------"
-  }
-  /Connected to/ {
-      print "Server IP: " $3
-  }
-  /User name okay, need password./ {
-      print "Status: Username accepted."
-  }
-  /User logged in, proceed./ {
-      print "Connection: Successful"
-  }
-  /Goodbye./ {
-      print "Connection: Closed"
-  }
-  END {
-    print "--------------------------------"
-    print "End of Connection Report"
-    print "\n"
-  }' 
-  exit 0
+    log "FTP connection test to meter succeeded." "SUCCESS" "$LOG_FILE"
+    # Optionally, print a formatted connection report to stdout
+    echo "$FTP_OUTPUT" | awk '
+    BEGIN {
+      print "\nFTP Connection Report"
+      print "--------------------------------"
+    }
+    /Connected to/ {
+        print "Server IP: " $3
+    }
+    /User name okay, need password./ {
+        print "Status: Username accepted."
+    }
+    /User logged in, proceed./ {
+        print "Connection: Successful"
+    }
+    /Goodbye./ {
+        print "Connection: Closed"
+    }
+    END {
+      print "--------------------------------"
+      print "End of Connection Report\n"
+    }'
+    exit 0
 fi
