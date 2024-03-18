64cb0ed9a107599aeb2de65c44abd652197a985d
nicolemah99 <nicolemah99@gmail.com>
2024-03-18 23:15:33 +0000

    add trap commands and cleanup function into data_pipeline

--- cli_meter/data_pipeline.sh
+++ cli_meter/data_pipeline.sh
@@ -9,6 +9,16 @@ config_path="./config/acep-data-streams/kea/events/sel735_config.yml"
 # Get the current date in YYYY-MM format
 date=$(date '+%Y-%m')
 
+# Initialize a variable to track the current event directory being processed
+current_event_dir=""
+
+cleanup() {
+    log "Cleanup triggered for: $current_event_dir" "warn"
+}
+
+# Setup trap for SIGINT and SIGTSTP
+trap cleanup SIGINT SIGTSTP
+
 # Make sure the output config file exists
 if [ -f "$config_path" ]; then
     log "Config file exists at: $config_path."
@@ -24,6 +34,8 @@ num_meters=$(yq '.meters | length' "$config_path")
 location=$(yq '.location' "$config_path")
 data_type=$(yq '.data_type' "$config_path")
 download_dir=$(yq '.download_directory' "$config_path")
+meter_type=$(yq '.meter_type' "$config_path")
+
 
 # Check for null or empty values
 [[ -z "$default_username" ]] && exit_with_error "Default username cannot be null or empty."
@@ -31,6 +43,9 @@ download_dir=$(yq '.download_directory' "$config_path")
 [[ -z "$num_meters" || "$num_meters" -eq 0 ]] && exit_with_error "Must have at least 1 meter in the config file."
 [[ -z "$location" ]] && exit_with_error "Location cannot be null or empty."
 [[ -z "$data_type" ]] && exit_with_error "Data type cannot be null or empty."
+[[ -z "$meter_type" ]] && exit_with_error "Meter type cannot be null or empty."
+
+
 
 # Adjust output_dir based on download_directory from config
 if [[ -n "$download_dir" ]]; then
@@ -61,7 +76,15 @@ for ((i = 0; i < num_meters; i++)); do
     export USERNAME=${meter_username:-$default_username}
     export PASSWORD=${meter_password:-$default_password}
 
+    # Before calling the download script, set current_event_dir
+    current_event_dir="$output_dir/$meter_id"
+
     # Execute download script
-    "meters/$meter_type/download.sh" "$meter_ip" "$output_dir/$meter_id"
+    "meters/$meter_type/download.sh" "$meter_ip" "$current_event_dir"
+
+    # Reset current_event_dir
+    current_event_dir=""
 
 done
+
+echo "Finished downloading events!!!!!!!!"
