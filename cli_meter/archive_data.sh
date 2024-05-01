24d70db55f049fb05dc823cb5b6effbaa70f6cf3
nicolemah99 <nicolemah99@gmail.com>
2024-05-01 20:37:00 +0000

    remove sshpass and added optional bandwith_limit to archive_config

--- cli_meter/archive_data.sh
+++ cli_meter/archive_data.sh
@@ -3,14 +3,15 @@
 # Use rsync to move data from local machine to DAS (Data Acquisition System)
 
 # Check command line arguments
-if [ $# -ne 4 ]; then
-    fail "Usage: $0 <src_dir> <dest_dir> <dest_host> <dest_user>"
+if [ $# -ne 5 ]; then
+    fail "Usage: $0 <src_dir> <dest_dir> <dest_host> <dest_user> <bwlimit>"
 fi
 
 src_dir=$1
 dest_dir=$2
 dest_host=$3
 dest_user=$4
+bwlimit=$5
 
 # Define source and destination directories
 current_dir=$(dirname "$(readlink -f "$0")")
@@ -22,28 +23,27 @@ source "$current_dir/commons.sh"
 if [ -d "$src_dir" ] && [ -n "$(ls -A $src_dir)" ]; then
     log "Attempting to transfer data from: $src_dir to $dest_dir on $dest_host as $dest_user"
 
-    # -a : Archive mode to preserve attributes and copy directories recursively
-    # -v : Verbose mode to see what rsync is doing
-    # -e : Allows you to specify the SSH command that rsync should use for data transport.
-    #      Here, you include all the necessary SSH options to ensure sshpass handles the password.
+    # Sync files from local to remote server using rsync
+    #   -a : Archive mode to preserve attributes and copy directories recursively
+    #   -v : Verbose mode to see what rsync is doing
+    #   --bwlimit : Limit I/O bandwidth (kbps)
+    #   --exclude : Exclude the 'working' directory
 
-    if [[ -z "$SSHPASS" ]]; then
-        fail "SSHPASS environment variable is not set."
-    fi
-
-    # Sync files from local to remote server using sshpass
-    rsync_output=$(sshpass -e rsync -av --exclude 'working' -e "ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password" $src_dir $dest_user@$dest_host:$dest_dir)
+    rsync_output=$(rsync -av --bwlimit=$bwlimit --exclude 'working' $src_dir $dest_user@$dest_host:$dest_dir)
     log "$rsync_output"
+
     # Check the status of the rsync command
     if [ $? -eq 0 ]; then
-        echo "$rsync_output" | grep -E '\.zip$' | while IFS= read -r line; do
+        echo "$rsync_output" | grep '.zip$' | while IFS= read -r line; do
 
-            # Extract the filename & event_id
-            filename=$(echo "$line" | awk -F/ '{print $NF}')
-            event_id=$(echo "$filename" | sed 's/\.zip$//')
+            # Extract the filename
+            filename=$(basename "$line")
+            # Extract the path without the filename
+            path=$(dirname "$line")
+            event_id=$(echo $filename | cut -d'_' -f1)
 
             # echo's to archive_pipeline.sh to parse and publish to MQTT
-            echo $event_id
+            echo $event_id,$filename,$path
 
         done
 
