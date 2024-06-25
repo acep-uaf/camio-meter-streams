70f435afdf2d85572e65104a8d51c87492c1aaa6
nicolemah99 <nicolemah99@gmail.com>
2024-06-25 23:16:51 +0000

    moved logic from create_metadata_yml.sh to generate_event_metadata, having two scripts seemed redundant since we removed json metadata

--- cli_meter/meters/sel735/generate_event_metadata.sh
+++ cli_meter/meters/sel735/generate_event_metadata.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # ==============================================================================
-# Script Name:        generate_event_metadata.sh
+# Script Name:        generate_and_create_metadata.sh
 # Description:        This script creates metadata for the specified event files
-#                     using environment variables and calls create_metadata_yml to 
-#                     output it in YML format.
+#                     using environment variables and outputs it in YML format.
 #
-# Usage:              ./generate_event_metadata.sh <event_id> <event_dir> <meter_id>
+# Usage:              ./generate_and_create_metadata.sh <event_id> <event_dir> <meter_id>
 #                     <meter_type> <event_timestamp> <download_timestamp>
-# Called by:          download.sh
 #
 # Arguments:
 #   event_id          Event ID
@@ -17,14 +15,15 @@
 #   event_timestamp   Original timestamp of the event
 #   download_timestamp Timestamp of when the files were downloaded
 #
-# Requirements:       create_metadata_yml.sh
+# Requirements:       commons.sh
 # ==============================================================================
 
 log "Creating metadata for event: $event_id"
 
 # Check if the correct number of arguments are passed
 if [ "$#" -ne 6 ]; then
-    fail "Usage: $0 <event_id> <event_dir> <meter_id> <meter_type> <event_timestamp> <download_timestamp>"
+    log "Usage: $0 <event_id> <event_dir> <meter_id> <meter_type> <event_timestamp> <download_timestamp>"
+    exit $EXIT_INVALID_ARGS
 fi
 
 event_id=$1
@@ -37,17 +36,45 @@ download_timestamp=$6
 # Directory where this script is located
 current_dir=$(dirname "${0}")
 
+# Function to create metadata in YML format and append the file's checksum
+create_metadata_yml() {
+    file=$1
+    event_dir=$2
+    meter_id=$3
+    meter_type=$4
+    event_timestamp=$5
+    download_timestamp=$6
+
+    event_id=$(basename "$event_dir")
+    filename=$(basename "$file")
+    metadata_file="${event_id}_metadata.yml"
+    metadata_path="$event_dir/$metadata_file"
+
+    checksum=$(md5sum "$file" | awk '{print $1}')
+    echo "$checksum $filename" >> "$event_dir/checksum.md5"
+
+    {
+        echo "- File: $filename"
+        echo "  DownloadedAt: \"$download_timestamp\""
+        echo "  MeterEventDate: \"$event_timestamp\""
+        echo "  MeterID: \"$meter_id\""
+        echo "  MeterType: \"$meter_type\""
+        echo "  EventID: \"$event_id\""
+        echo "  DataLevel: \"level0\""
+        echo "  Checksum: \"$checksum\""
+    } >> "$metadata_path" && log "Metadata generated for: $filename" || {
+        log "Error generating metadata for: $filename"
+        exit $EXIT_METADATA_FAIL
+    }
+}
+
 # Loop through each file in the event directory
 for file in "$event_dir"/*; do
-    if [ -f "$file" ] && [ -s "$file" ]; then
-        # Source and check create_metadata_yml.sh
-        source "$current_dir/create_metadata_yml.sh" "$file" "$event_dir" "$meter_id" "$meter_type" "$event_timestamp" "$download_timestamp"
-
-        if [ $? -ne 0 ]; then
-            fail "create_metadata_yml.sh failed for: $file"
-        fi
-
-    else
-        fail "No file found for $file"
-    fi
+    [ -f "$file" ] && [ -s "$file" ] && {
+        log "Processing file: $file"
+        create_metadata_yml "$file" "$event_dir" "$meter_id" "$meter_type" "$event_timestamp" "$download_timestamp" || exit $EXIT_METADATA_FAIL
+    } || {
+        log "File not found or is empty: $event_dir/$file"
+        exit $EXIT_FILE_NOT_FOUND
+    }
 done
