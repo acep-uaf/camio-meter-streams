51c59bf4ecbe25cc890247850fbc86648609ca6b
nicolemah99 <nicolemah99@gmail.com>
2024-07-18 20:41:31 +0000

    update id to event_id in json message file

--- cli_meter/meters/sel735/create_message.sh
+++ cli_meter/meters/sel735/create_message.sh
@@ -3,11 +3,11 @@
 # Script Name:        create_message.sh
 # Description:        This script creates a .message file with a JSON payload.
 #
-# Usage:              ./create_message.sh <id> <zip_filename> <path>
+# Usage:              ./create_message.sh <event_id> <zip_filename> <path>
 #                     <data_type> <output_dir>
 #
 # Arguments:
-#   id                ID
+#   event_id                ID
 #   zip_filename      Name of the zipped file
 #   path              Path to file
 #   data_type         Type of data
@@ -21,9 +21,9 @@ script_name=$(basename "$0")
 source "$current_dir/../../common_utils.sh" 
 
 # Check for exactly 5 arguments
-[ "$#" -ne 5 ] && failure $STREAMS_INVALID_ARGS "Usage: $script_name <id> <zip_filename> <path> <data_type> <output_dir>"
+[ "$#" -ne 5 ] && failure $STREAMS_INVALID_ARGS "Usage: $script_name <event_id> <zip_filename> <path> <data_type> <output_dir>"
 
-id="$1"
+event_id="$1"
 zip_filename="$2"
 path="$3"
 data_type="$4"
@@ -32,11 +32,11 @@ message_file="$output_dir/${zip_filename}.message"
 
 # Create the JSON payload
 json_payload=$(jq -n \
-    --arg id "$id" \
+    --arg eid "$event_id" \
     --arg fn "$zip_filename" \
     --arg pth "$path" \
     --arg dt "$data_type" \
-    '{id: $id, filename: $fn, path: $pth, data_type: $dt}')
+    '{event_id: $eid, filename: $fn, path: $pth, data_type: $dt}')
 
 # Write the JSON payload to the .message file
 echo "$json_payload" > "$message_file" && log "Created message file: $message_file" || warning "Failed to write to message file: $message_file"
